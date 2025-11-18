fabd53c1c7b968d8db0c792a7486a3aa396d390d
Amitav <amitavrchy01@gmail.com>
2025-11-18 15:06:49 +0600

    trying

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,9 @@ const app = express();
 
 
 app.use(cors({
-    origin: ["http://localhost:5173","http://localhost:5174", "https://tortuga7.com"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://tortuga7.com"],
+    methods: "GET,POST,PUT,PATCH,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
     credentials: true
 }));
 app.use(express.json());
