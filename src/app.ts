e671a46d4d90999b0d8fd9be431d6dc15dec71d5
Amitav <amitavrchy01@gmail.com>
2025-11-19 19:46:03 +0600

    fixed

--- src/app.ts
+++ src/app.ts
@@ -17,11 +17,14 @@ const app = express();
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://tortuga7.com"],
-    methods: "GET,POST,PUT,PATCH,DELETE",
-    allowedHeaders: "Content-Type, Authorization",
-    credentials: true
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://tortuga7.com"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
+
+app.options('*', cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
